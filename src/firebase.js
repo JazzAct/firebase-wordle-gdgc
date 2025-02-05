17734b71c492407e6a91b6e77b1bb200e6e90405
JazzAct <84248962+JazzAct@users.noreply.github.com>
2025-02-05 13:07:09 -0800

    User friendly

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD42GimcPhm2Z4JuS9Bevn0kpsIEqdYcAQ",
-  authDomain: "wordle-leaderboard-9a182.firebaseapp.com",
-  projectId: "wordle-leaderboard-9a182",
-  storageBucket: "wordle-leaderboard-9a182.firebasestorage.app",
-  messagingSenderId: "462143735136",
-  appId: "YOUR_1:462143735136:web:9dec210a5462afee0cca05APP_ID"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-Id",
+  storageBucket: "your-storageBucket",
+  messagingSenderId: "your-messaging-senderID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
